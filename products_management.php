d4e72d73c4cb714cc2476c989cbfebe05a9161f2
isaacsagal <sagaltorresisaac@gmail.com>
2026-03-25 17:54:07 +0100

    update

--- products_management.php
+++ products_management.php
@@ -1,3 +1,14 @@
+<?php
+//Siempre iniciar sesion por si no está iniciada
+// Always start the session in case it hasn't been started
+session_start();
+//Utilizar  los archivos de funciones y conexión a BBDD
+    require_once 'functions.php';
+    require_once 'db_erp.php';
+//SECURITY: Restrict access to administrators only
+//SEGURIDAD: Restringir el acceso solo a administradores
+require_role(['admin','obrador']);
+?>
 <!DOCTYPE html>
 <!--
 Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -9,8 +20,6 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHPWebPage.php to 
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        
     </body>
 </html>
